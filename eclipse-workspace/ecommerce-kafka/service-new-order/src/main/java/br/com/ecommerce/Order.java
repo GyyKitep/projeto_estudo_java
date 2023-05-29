e7083b4ff6d036c7182a31f317803f223240f5cf
Geovanny Gonsales <geovanny.gonsales.dev@gmail.com>
2023-05-28 21:30:36 -0300

    modularização do projeto ecommerce-kafka

---
+++ eclipse-workspace/ecommerce-kafka/service-new-order/src/main/java/br/com/ecommerce/Order.java
@@ -0,0 +1,18 @@
+package br.com.ecommerce;
+
+import java.math.BigDecimal;
+
+public class Order {
+	
+	private final String userId, orderId;
+	private final BigDecimal amount;
+	
+	public Order(String userId, String orderId, BigDecimal amount) {
+		this.userId = userId;
+		this.orderId = orderId;
+		this.amount = amount;
+	}
+	
+	
+
+}
